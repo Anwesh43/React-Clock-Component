b9596ce99593195cd59499f903bfca89dd7f3bc2
Anwesh43 <anweshthecool0@gmail.com>
2017-04-08 18:35:52 +0530

    Using Store to hande state in Clock Component

--- components/Clock.js
+++ components/Clock.js
@@ -1,28 +1,49 @@
 import React,{Component} from 'react'
+import Store,{createStore} from '../Store'
+
+const addZeroIfReq = (digit)=>{
+    return digit>=10?digit:"0"+digit
+}
+const setTime = ()=>{
+    const date = new Date()
+    var h = date.getHours()
+    const period = h>=12?"PM":"AM"
+    h = h>=12?h-5:h
+    const hStr = addZeroIfReq(h)
+    const m = date.getMinutes()
+    const mStr = addZeroIfReq(m)
+    const s = date.getSeconds()
+    const sStr = addZeroIfReq(s)
+    const blinkStr = ":"
+    return {hStr,mStr,sStr,period,blinkStr}
+}
+const dateReducer = (state={},action) => {
+    var newState = {}
+    switch(action.type) {
+        case "UPDATE":
+            newState = setTime()
+            break
+        default:
+            break
+    }
+    return newState
+}
 export default class Clock extends Component {
     constructor(props) {
         super(props)
-        this.state = this.setTime()
+        this.state = setTime()
     }
-    addZeroIfReq(digit) {
-        return digit>=10?digit:"0"+digit
-    }
-    setTime() {
-        const date = new Date()
-        var h = date.getHours()
-        const period = h>=12?"PM":"AM"
-        const hStr = this.addZeroIfReq(h)
-        const m = date.getMinutes()
-        const mStr = this.addZeroIfReq(m)
-        const s = date.getSeconds()
-        const sStr = this.addZeroIfReq(s)
-        const blinkStr = ":"
-        return {hStr,mStr,sStr,period,blinkStr}
-    }
+
+
     componentDidMount() {
+        const store = createStore(dateReducer)
+        store.subscribe(()=>{
+            this.setState(store.getState())
+        })
         setInterval(()=>{
-            this.setState(this.setTime())
+            store.dispatch({type:'UPDATE'})
         },1000)
+
     }
     render() {
         const w = window.innerWidth,h = window.innerHeight
